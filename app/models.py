b8b27dadb8c8f9600a13b3bbbc4363d8f7983993
abhi-Sharma-Raun <abhinandantyagiraun@gmail.com>
2025-12-31 23:22:24 +0530

    added some clarification comment for thread_id and is_active

--- app/models.py
+++ app/models.py
@@ -8,9 +8,9 @@ class ChatSession(Base):
     __tablename__="chatsession"
     session_id = Column(UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()"))
     thread_id = Column(UUID(as_uuid=True), nullable=False)
-    is_active = Column(Boolean, server_default=text("false"), nullable=False)
+    is_active = Column(Boolean, server_default=text("false"), nullable=False)   # This is the sorce of truth if a thread/chat is active and can be resumed or not
     session_created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False)
     thread_created_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False)
     thread_closed_at = Column(TIMESTAMP(timezone=True), nullable=True)
-    thread_last_used_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False)
+    thread_last_used_at = Column(TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False)   # It is only treated as a heartbeat not proof of success
     
