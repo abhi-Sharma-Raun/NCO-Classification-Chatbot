4e14ae19c412823b4c91d0d20d5ff143034c5f02
abhi-Sharma-Raun <abhinandantyagiraun@gmail.com>
2026-01-06 03:32:01 +0530

    added some extra envicornment variables to support the neon and supabase database

--- app/config.py
+++ app/config.py
@@ -1,14 +1,21 @@
 from pydantic_settings import BaseSettings
-from pathlib import Path
 
 class Settings(BaseSettings):
+    checkpointer_db_password: str
+    checkpointer_db_host: str
+    checkpointer_db_username: str
+    checkpointer_db_dbname: str
     database_password: str
     database_region: str
+    database_host: str
+    database_username: str
     groq_api_key: str
     langsmith_tracing: bool
     langsmith_endpoint: str
     langsmith_api_key: str
     langsmith_project: str
+    allowed_url1: str
+    allowed_url2: str
     
     class Config:
         env_file = ".env"
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import create_session, create_chat, start_chat, resume_chat
+from .config import settings
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -13,11 +14,11 @@ app.include_router(create_session.router)
 app.include_router(create_chat.router)
 app.include_router(start_chat.router)
 app.include_router(resume_chat.router)
-
-
+        
+             
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],
+    allow_origins = [f"{settings.allowed_url1}", f"{settings.allowed_url2}"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
